efa234ed5752a2ef2c83463d2a8bd3c7fd9216ad
vishwa775 <vishwaamagi@gmail.com>
2025-11-27 23:02:20 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+# Addition and Subtraction in Python
+
+# Taking inputs
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Performing operations
+addition = a + b
+subtraction = a - b
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
